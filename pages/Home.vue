80c00c746b40812ecda8b268347e5ec042af6c56
Daksh Miglani <hello@dak.sh>
2017-09-20 17:34:45 +0530

    it works

--- pages/Home.vue
+++ pages/Home.vue
@@ -4,21 +4,6 @@
     @click="open3">
     Success
   </el-button>
-  <el-button
-    plain
-    @click="open4">
-    Warning
-  </el-button>
-  <el-button
-    plain
-    @click="open5">
-    Info
-  </el-button>
-  <el-button
-    plain
-    @click="open6">
-    Error
-  </el-button>
 </template>
 
 <script>
@@ -29,28 +14,6 @@
           title: 'Success',
           message: 'This is a success message',
           type: 'success'
-        });
-      },
-
-      open4() {
-        this.$notify({
-          title: 'Warning',
-          message: 'This is a warning message',
-          type: 'warning'
-        });
-      },
-
-      open5() {
-        this.$notify.info({
-          title: 'Info',
-          message: 'This is an info message'
-        });
-      },
-
-      open6() {
-        this.$notify.error({
-          title: 'Error',
-          message: 'This is an error message'
         });
       }
     }
