8d55bcc2b0f7b9f16dde024c8b31a5f3f5a6070d
Daksh <hello@dak.sh>
2017-10-11 23:47:09 +0530

    Fix route shit.

--- template/app.js
+++ template/app.js
@@ -8,14 +8,14 @@ import routes from './handler/routes'
 import handleSock from './handler/socket'
 import config from './handler/config'
 import raven from './handler/raven'
+import database from './database/index'
+import verify from './email/verify'
 
 var resolve = file => path.resolve(__dirname, file),
    redirects = require('./router/301.json'),
    isProd = process.env.NODE_ENV === 'production',
    useMicroCache = process.env.MICRO_CACHE !== 'false',
-   serverInfo =
-    `express/${require('express/package.json').version} ` +
-    `vue-server-renderer/${require('vue-server-renderer/package.json').version}`
+   serverInfo = `Driffle-Server Powered with Server-Side-Rendering`
 
 var app = express(),
    server = http.createServer(app),
@@ -23,7 +23,10 @@ var app = express(),
    io = socketIO(server),
    params = require('./handler/params')
 
-// Daksh Miglani The NOOBs IMPLEMENTATION;
+// run db
+const connection = database()
+
+// render functions for Vue 2 SSR
 var createRenderer = require('./handler/createRender')
 
 let renderer, readyPromise;
@@ -54,50 +57,60 @@ const microCache = LRU({
 })
 const isCacheable = req => useMicroCache
 
+let nonVueRoutes = []
+
+for(let i in routes.stack) {
+  nonVueRoutes.push(routes.stack[i]["route"]["path"].toLowerCase())
+}
+
 let render = (req, res) => {
-  const s = Date.now()
-
-  res.setHeader("Content-Type", "text/html")
-  res.setHeader("Server", serverInfo)
-
-  const handleError = err => {
-    if (err && err.code === 404) {
-      res.status(404).end('404 | Page Not Found')
-    } else {
-      // Render Error Page or Redirect
-      res.status(500).end('500 | Internal Server Error')
-      console.error(`error during render : ${req.url}`)
-      console.error(err.stack)
-    }
-  }
-
-  const cacheable = isCacheable(req)
-  if (cacheable) {
-    const hit = microCache.get(req.url)
-    if (hit) {
-      if (!isProd) {
-        console.log(`cache hit!`)
+  if(nonVueRoutes.indexOf(req.url.split("?")[0].toLowerCase()) === -1) {
+      const s = Date.now()
+      
+      res.setHeader("Content-Type", "text/html")
+      res.removeHeader('X-Powered-By');
+    
+      const handleError = err => {
+        if (err && err.code === 404) {
+          res.status(404).end('404 | Page Not Found')
+        } else {
+          // Render Error Page or Redirect
+          res.status(500).end('500 | Internal Server Error')
+          console.error(`error during render : ${req.url}`)
+          console.error(err.stack)
+        }
       }
-      return res.end(hit)
-    }
+    
+      const cacheable = isCacheable(req)
+      if (cacheable) {
+        const hit = microCache.get(req.url)
+        if (hit) {
+          if (!isProd) {
+            console.log(`cache hit!`)
+          }
+          return res.end(hit)
+        }
+      }
+    
+      const context = {
+        title: 'Driffle - Read, Write, Create Content with Fun.', // default title
+        url: req.url
+      }
+      renderer.renderToString(context, (err, html) => {
+        if (err) {
+          return handleError(err)
+        }
+        res.end(html)
+        if (cacheable) {
+          microCache.set(req.url, html)
+        }
+        if (!isProd) {
+          console.log(`whole request: ${Date.now() - s}ms`)
+        }
+      })
+  } else {
+    // don't do shit.
   }
-
-  const context = {
-    title: 'Quantized - Vue SSR Toolkit', // default title
-    url: req.url
-  }
-  renderer.renderToString(context, (err, html) => {
-    if (err) {
-      return handleError(err)
-    }
-    res.end(html)
-    if (cacheable) {
-      microCache.set(req.url, html)
-    }
-    if (!isProd) {
-      console.log(`whole request: ${Date.now() - s}ms`)
-    }
-  })
 }
 // handle app.use functions
 params(app)
@@ -112,7 +125,6 @@ app.use('/', routes)
 app.get('*', isProd ? render : (req, res) => {
   readyPromise.then(() => render(req, res))
 })
-
 // socket.io
 io.on('connection', (socket) => {
   handleSock(socket);
